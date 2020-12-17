4f4013652ce6c3e60b4999b660c848737a4997fe
HoduGwaja <honeybutter9250@gmail.com>
2020-12-17 23:33:16 +0900

    [commit | 2020.12.17 | styling list page]

---
+++ src/components/common/SubInfo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled, {css} from 'styled-components';
+import {Link} from 'react-router-dom';
+import palette from '../../lib/styles/palette';
+
+const SubInfoBlock = styled.div`
+    ${props =>
+        props.hasMarginTop &&
+        css`
+            margin-top : 1rem;
+    `}
+    color : ${palette.gray[4]};
+    padding-left : 0.25rem;
+    padding-right : 0.25rem;
+    content : '\\B7';
+`;
